839801c69eddc5e9ad2ac8e29c909c88861a12ec
ShevtsovDenis <shevcov.organika@yandex.ru>
2022-11-10 21:59:52 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная работа 11. Структуры/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_11.Структуры
+{
+    struct Line
+    {
+        double b;
+        double k;
+
+        public Line(double b, double k)
+        {
+         this.b = b;
+         this.k = k;
+        }
+
+        public string Root()
+        {
+            double x = -b / k;
+            if (k==0)
+            {
+                Console.WriteLine("коэффициент k не может быть равен 0");
+            }
+            return $"Решение - {x:f2}";
+        }
+    }
+}
---
+++ Самостоятельная работа 11. Структуры/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_11.Структуры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение коеффициентов b и k. Для решения линейного уравнения  0=kx+b");
+            double b_ = Convert.ToDouble(Console.ReadLine());
+            double k_ = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(b_, k_);
+            Console.WriteLine(line.Root());
+            Console.ReadKey();
+        }
+    }
+}
