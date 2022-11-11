8c2bb0a85f8f8fc23e4facf8e405c5e3a7a74527
shevcov.organika@yandex.ru <shevcov.organika@yandex.ru>
2022-11-11 13:29:34 +0300

    Исправлен метод

--- Самостоятельная работа 11. Структуры/Line.cs
+++ Самостоятельная работа 11. Структуры/Line.cs
@@ -7,24 +7,25 @@ using System.Threading.Tasks;
 namespace Самостоятельная_работа_11.Структуры
 {
     struct Line
-    {
+    {//Поля
         double b;
         double k;
-
+        //конструктор
         public Line(double b, double k)
         {
-         this.b = b;
-         this.k = k;
+            this.b = b;
+            this.k = k;
         }
-
+        //метод
         public string Root()
         {
             double x = -b / k;
-            if (k==0)
-            {
-                Console.WriteLine("коэффициент k не может быть равен 0");
-            }
-            return $"Решение - {x:f2}";
+            if (k == 0 && b == 0)
+                return "Неверные значения коэффициентов. Деление 0 на 0";
+            else if (k == 0)
+                return "Коэффициент k не может быть равен 0";
+            else
+                return $"Решение - {x:f2}";
         }
     }
 }
--- Самостоятельная работа 11. Структуры/Program.cs
+++ Самостоятельная работа 11. Структуры/Program.cs
@@ -13,7 +13,7 @@ namespace Самостоятельная_работа_11.Структуры
             Console.WriteLine("Введите значение коеффициентов b и k. Для решения линейного уравнения  0=kx+b");
             double b_ = Convert.ToDouble(Console.ReadLine());
             double k_ = Convert.ToDouble(Console.ReadLine());
-
+            //экземпляр структуры
             Line line = new Line(b_, k_);
             Console.WriteLine(line.Root());
             Console.ReadKey();
